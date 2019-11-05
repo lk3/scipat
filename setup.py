5e63784af64fc3c489165e5cf3568a5bb1b691a5
Luciano Kay <luciano@kay.com.ar>
2019-11-05 10:08:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setup(name='scipat',
-      version='1.0.0',
-      description='Generates dataset from USPTO patent records',
-      url='https://github.com/lk3/scipat',
-      author='Luciano Kay',
-      author_email='luciano@kay.com.ar',
-      license='GNU GPLv3',
-      classifiers=[''],
-      keywords=[''],
-      packages=['scipat'],
-      install_requires=['bs4', 'requests', 'pandas', 'selenium'],
-      python_requires='>=3',
-      zip_safe=False,
-      long_description=long_description,
-      long_description_content_type='text/markdown')
+setup(
+    name='scipat',
+    version='1.0.0',
+    description='Generates dataset from USPTO patent records',
+    url='https://github.com/lk3/scipat',
+    author='Luciano Kay',
+    author_email='luciano@kay.com.ar',
+    license='GNU GPLv3',
+    classifiers=[''],
+    keywords=[''],
+    packages=['scipat'],
+    install_requires=['bs4', 'requests', 'pandas', 'selenium'],
+    python_requires='>=3',
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown')
